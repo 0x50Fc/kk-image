f6dc4b72f575d17d6a1441be27d31720e8632891
hailong11 <hailong11@staff.weibo.com>
2018-05-17 15:55:32 +0800

    fix bug

---
+++ kk-image/src/main/java/cn/kkmofang/image/ImageCache.java
@@ -0,0 +1,86 @@
+package cn.kkmofang.image;
+
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.SoftReference;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by hailong11 on 2018/5/17.
+ */
+
+public class ImageCache {
+
+    private final Map<String,SoftReference<Drawable>> _images = new TreeMap<>();
+
+    public Drawable getImage(File file) {
+        Drawable v = getImage(file.getAbsoluteFile());
+        if(v == null) {
+            try {
+                FileInputStream in = new FileInputStream(file);
+                try {
+                    v = getImage(in, file.getAbsolutePath());
+                }
+                finally {
+                    in.close();
+                }
+            } catch (IOException e) {
+                Log.d("kk",Log.getStackTraceString(e));
+            }
+        }
+        return v;
+    }
+
+    public Drawable getImage(String key) {
+
+        if(key == null) {
+            return null;
+        }
+
+        if(_images.containsKey(key)) {
+            SoftReference<Drawable> v = _images.get(key);
+            Drawable vv = v.get();
+            if(vv == null) {
+                _images.remove(key);
+            } else {
+                return vv;
+            }
+        }
+
+        return null;
+    }
+
+    public Drawable getImage(InputStream in, String key) {
+
+        if(key == null) {
+            return null;
+        }
+
+        if(_images.containsKey(key)) {
+            SoftReference<Drawable> v = _images.get(key);
+            Drawable vv = v.get();
+            if(vv == null) {
+                _images.remove(key);
+            } else {
+                return vv;
+            }
+        }
+
+        Drawable vv = Drawable.createFromStream(in,key);
+
+        if(vv != null) {
+            _images.put(key,new SoftReference<>(vv));
+        }
+
+        return vv;
+    }
+
+    public final static ImageCache main = new ImageCache();
+}
